0ededc988a5f92dc45c6ae83a43ec391f0e44ff6
d4Rk <darkdevilx@gmx.net>
2023-10-04 10:01:38 +0200

    Initial Commit

---
+++ BearAppSync/BearAppSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  BearAppSyncApp.swift
+//  BearAppSync
+//
+//  Created by d4Rk on 04.10.23.
+//
+
+import SwiftUI
+
+@main
+struct BearAppSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
